8da067be5c9ec8605006e94e6cdb51370b2d7ac4
Sylvain Maillard <sylvain.maillard6@gmail.com>
2015-03-29 16:19:41 +0200

    Basic application commit

---
+++ Desktop/src/main/java/logo3d/desktop/Logo3d.java
@@ -0,0 +1,38 @@
+package logo3d.desktop;
+
+import com.jme3.app.SimpleApplication;
+import org.slf4j.Logger;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
+import java.util.logging.LogManager;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Created by SylvainMaillard on 29/03/2015.
+ */
+public class Logo3d extends SimpleApplication {
+
+    private static final Logger LOG = getLogger(Logo3d.class);
+
+    public static void main(String[] args) {
+        bridgeJUL();
+        LOG.debug("Starting...");
+        Logo3d logo3d = new Logo3d();
+        logo3d.start();
+    }
+
+    /**
+     * remove default Java logging util, replace with slf4j
+     */
+    private static void bridgeJUL() {
+        LogManager.getLogManager().reset();
+        SLF4JBridgeHandler.install();
+        java.util.logging.Logger.getLogger("global").setLevel(java.util.logging.Level.FINEST);
+    }
+
+    @Override
+    public void simpleInitApp() {
+
+    }
+}
